d8a3f09ee8bb7c0f7d1b8cc59521c2affa676a02
wzc520pyfm <1528857653@qq.com>
2024-08-10 19:10:09 +0800

    chore: update deps

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 import Inspect from 'vite-plugin-inspect'
-import Unplugin from '../dist/vite'
+import visualizer from 'unplugin-visualizer2/vite'
 
 export default defineConfig({
   plugins: [
     Inspect(),
-    Unplugin(),
-    // visualizer({
-    //   // template: 'flamegraph',
-    //   // template: 'list',
-    //   template: 'sunburst',
-    //   // template: 'treemap',
-    //   // template: 'network',
-    //   // template: 'raw-data',
-    // }),
+    visualizer({
+      // template: 'flamegraph',
+      // template: 'list',
+      template: 'sunburst',
+      // template: 'treemap',
+      // template: 'network',
+      // template: 'raw-data',
+    }),
   ],
 })
